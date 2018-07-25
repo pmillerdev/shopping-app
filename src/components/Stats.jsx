1e8642de86d087f91beec6aa05f74c0a91815d99
Pete <petermiller89@hotmail.co.uk>
2018-07-25 17:17:13 +0100

    feat: calculate statistics for shopping bag items

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,24 +3,38 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Stats extends Component {
+  getTotalCost = items => {
+    let totalCost = 0;
+    items.forEach(item => totalCost += item.cost)
+    return totalCost;
+  }
+  
+  getTotalCalories = items => {
+    let totalCalories = 0;
+    items.forEach(item => totalCalories += item.calories)
+    return totalCalories;
+  }
+  
+  getTotalWeight = items => {
+    let totalWeight = 0;
+    items.forEach(item => totalWeight += item.weight)
+    return totalWeight;
+  };  
+
   render() {
+    const bag = this.props.shoppingBag;
     return (
       <div className="col-md-4">
       <h2 className="text-center">Shopping Statistics</h2>
         <ul className="list-group">
-          <li className="list-group-item">Cost - £</li>
-          <li className="list-group-item">Calories - kcal</li>
-          <li className="list-group-item">Weight - mg</li>
+          <li className="list-group-item">Cost - £{this.getTotalCost(bag)}</li>
+          <li className="list-group-item">Calories - {this.getTotalCalories(bag)} kcal</li>
+          <li className="list-group-item">Weight - {this.getTotalWeight(bag)} mg</li>
         </ul>        
       </div>
     )
   }
 }
-
-/*
-const getTotalCost = items => {
-}
-*/
 
 const mapStateToProps = state => {
   return {
